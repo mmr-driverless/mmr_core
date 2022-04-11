880e3b722c1be6486068eac963cb252de28ca0b8
Stefano Calabretti <StefanoCalabretti@outlook.it>
2022-04-11 16:09:11 +0200

    Add brake project

---
+++ brake/Debug/Drivers/MMR_CAN/Src/mmr_can_events.d
@@ -0,0 +1,83 @@
+Drivers/MMR_CAN/Src/mmr_can_events.o: \
+ ../Drivers/MMR_CAN/Src/mmr_can_events.c ../Drivers/MMR_CAN/Inc/mmr_can.h \
+ ../Drivers/MMR_CAN/Inc/mmr_can_includes.h ../Core/Inc/main.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal.h \
+ ../Core/Inc/stm32f3xx_hal_conf.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_def.h \
+ ../Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f3xx.h \
+ ../Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f302x8.h \
+ ../Drivers/CMSIS/Include/core_cm4.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc_ex.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio_ex.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_exti.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma_ex.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_cortex.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_adc.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_adc_ex.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_can.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash_ex.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c_ex.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr_ex.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_tim.h \
+ ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_tim_ex.h \
+ ../Drivers/MMR_CAN/Inc/mmr_can_header.h \
+ ../Drivers/MMR_CAN/Inc/mmr_can_util.h \
+ ../Drivers/MMR_CAN/Inc/mmr_can_binary_literals.h \
+ ../Drivers/MMR_CAN/Inc/mmr_can_message_id.h \
+ ../Drivers/MMR_CAN/Inc/mmr_can_types.h \
+ ../Drivers/MMR_CAN/Inc/mmr_can_optimize.h \
+ ../Drivers/MMR_CAN/Inc/mmr_can_scs.h ../Drivers/MMR_CAN/Inc/mmr_can.h \
+ ../Drivers/MMR_CAN/Inc/mmr_can_events.h
+../Drivers/MMR_CAN/Inc/mmr_can.h:
+../Drivers/MMR_CAN/Inc/mmr_can_includes.h:
+../Core/Inc/main.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal.h:
+../Core/Inc/stm32f3xx_hal_conf.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_def.h:
+../Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f3xx.h:
+../Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f302x8.h:
+../Drivers/CMSIS/Include/core_cm4.h:
+../Drivers/CMSIS/Include/cmsis_version.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+../Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc_ex.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio_ex.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_exti.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma_ex.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_cortex.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_adc.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_adc_ex.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_can.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash_ex.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c_ex.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr_ex.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_tim.h:
+../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_tim_ex.h:
+../Drivers/MMR_CAN/Inc/mmr_can_header.h:
+../Drivers/MMR_CAN/Inc/mmr_can_util.h:
+../Drivers/MMR_CAN/Inc/mmr_can_binary_literals.h:
+../Drivers/MMR_CAN/Inc/mmr_can_message_id.h:
+../Drivers/MMR_CAN/Inc/mmr_can_types.h:
+../Drivers/MMR_CAN/Inc/mmr_can_optimize.h:
+../Drivers/MMR_CAN/Inc/mmr_can_scs.h:
+../Drivers/MMR_CAN/Inc/mmr_can.h:
+../Drivers/MMR_CAN/Inc/mmr_can_events.h:
---
+++ brake/Debug/Drivers/MMR_CAN/Src/mmr_can_header.d
@@ -0,0 +1,14 @@
+Drivers/MMR_CAN/Src/mmr_can_header.o: \
+ ../Drivers/MMR_CAN/Src/mmr_can_header.c \
+ ../Drivers/MMR_CAN/Inc/mmr_can_header.h \
+ ../Drivers/MMR_CAN/Inc/mmr_can_util.h \
+ ../Drivers/MMR_CAN/Inc/mmr_can_binary_literals.h \
+ ../Drivers/MMR_CAN/Inc/mmr_can_message_id.h \
+ ../Drivers/MMR_CAN/Inc/mmr_can_util.h \
+ ../Drivers/MMR_CAN/Inc/mmr_can_optimize.h
+../Drivers/MMR_CAN/Inc/mmr_can_header.h:
+../Drivers/MMR_CAN/Inc/mmr_can_util.h:
+../Drivers/MMR_CAN/Inc/mmr_can_binary_literals.h:
+../Drivers/MMR_CAN/Inc/mmr_can_message_id.h:
+../Drivers/MMR_CAN/Inc/mmr_can_util.h:
+../Drivers/MMR_CAN/Inc/mmr_can_optimize.h:
