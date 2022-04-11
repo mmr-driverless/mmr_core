49cb531ff1ed35215f0c4443c2c0d2ea6fb21811
Stefano Calabretti <StefanoCalabretti@outlook.it>
2022-04-11 16:21:06 +0200

    Remove Debug directory

--- brake/Debug/Drivers/MMR_CAN/Src/mmr_can_message_id.d
+++
@@ -1,6 +0,0 @@
-Drivers/MMR_CAN/Src/mmr_can_message_id.o: \
- ../Drivers/MMR_CAN/Src/mmr_can_message_id.c \
- ../Drivers/MMR_CAN/Inc/mmr_can_message_id.h \
- ../Drivers/MMR_CAN/Inc/mmr_can_binary_literals.h
-../Drivers/MMR_CAN/Inc/mmr_can_message_id.h:
-../Drivers/MMR_CAN/Inc/mmr_can_binary_literals.h:
