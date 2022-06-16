cd10081ea619f03ac778275ae76d1171f66723b3
Stefano Calabretti <StefanoCalabretti@outlook.it>
2022-06-16 21:16:19 +0200

    Use autonomous state logic

--- test/launch_control/Core/Src/main.c
+++ test/launch_control/Core/Src/main.c
@@ -57,6 +57,8 @@ uint32_t ADC_value[2];
 uint32_t dacValue = DAC_0;
 int gear = 0;
 int nMot = 0;
+
+int isLaunchControlSetCnt = 0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -68,6 +70,80 @@ static void MX_DAC_Init(void);
 static void MX_TIM2_Init(void);
 static void MX_ADC1_Init(void);
 /* USER CODE BEGIN PFP */
+
+typedef enum ClutchState {
+  START,
+  PULL,
+  PULLED,
+  GEAR_NOT_SET,
+  GEAR_CHANGING,
+  GEAR_CHANGED,
+  GEAR_NOT_CHANGED,
+  LAUNCH_CONTROL_SET,
+  APPS_30,
+  RELEASE,
+  RELEASED,
+  DONE,
+} ClutchState;
+
+typedef enum GearN {
+  GEAR_N_OFF = GPIO_PIN_RESET,
+  GEAR_N_ON = GPIO_PIN_SET,
+} GearN;
+
+static ClutchState state = START;
+static HalStatus status = HAL_OK;
+
+
+static bool changeGearStop = false;
+static bool sendLaunch = false;
+static bool firstrun=0;
+
+
+static uint8_t changeGear(bool reset) {
+  static int start = 0;
+  if (reset)
+    start = uwTick;
+
+  if (changeGearStop || gear) {
+    HAL_GPIO_WritePin(GEAR_CHANGE_GPIO_Port, GEAR_CHANGE_Pin, GEAR_N_OFF);
+    return true;
+  }
+
+  bool isWithin50ms = uwTick - start < 350;
+  uint8_t out = isWithin50ms
+    ? GEAR_N_ON
+    : GEAR_N_OFF;
+
+  HAL_GPIO_WritePin(GEAR_CHANGE_GPIO_Port, GEAR_CHANGE_Pin, out);
+  return out == GEAR_N_ON;
+}
+
+
+
+static bool waitMs(bool reset, int ms) {
+  static int start = 0;
+  if (reset) {
+    start = uwTick;
+  }
+
+  return uwTick - start >= ms;
+}
+
+static uint16_t readGear(CanRxBuffer buffer) {
+  return buffer[5] << 8 | buffer[4];
+}
+
+static uint16_t readRPM(CanRxBuffer buffer) {
+  return buffer[1] << 8 | buffer[0];
+}
+
+static uint8_t readLaunch(CanRxBuffer buffer) {
+  int bytes7_8 = (buffer[7] << 8 | buffer[6]);
+  int isLaunchControlBitHigh = (bytes7_8 & 0x8) == 0x8;
+
+  return isLaunchControlBitHigh ? +1 : -1;
+}
 
 /* USER CODE END PFP */
 
@@ -136,13 +212,6 @@ int main(void)
       .noExtId = true,
       .header = MMR_CAN_HeaderFromBits(0x628),
   };
-  uint8_t _launchControlStopData[8] = {0x0};
-  MmrCanPacket launchControlStopPacket = {
-        .data = _launchControlStopData,
-        .length = 8,
-        .noExtId = true,
-        .header = MMR_CAN_HeaderFromBits(0x628),
-    };
 
   int clutchMsgStart = uwTick;
   int clutchMsgCnt = 0;
@@ -151,8 +220,6 @@ int main(void)
   int launchControlStop = 0;
   int launchControlCnt = 0;
 
-  bool waitForStateChange = false;
-  int stateChangeStart = 0;
 
   while (1)
   {
@@ -165,188 +232,132 @@ int main(void)
         gear = readGear(buffer);
         nMot = readRPM(buffer);
         break;
+
+      case MMR_CAN_MESSAGE_ID_ECU_ENGINE_FN2:
+        isLaunchControlSetCnt += readLaunch(buffer);
+        break;
       }
     }
 
-    if (isManual){
-    	sendLaunchM=true;
-      dacValue = ADC_value[0];
-if(gear==1){
-    if ((nMot > 1500 && sendLaunchM && uwTick - launchControlStart > 25)) {
-          status = MMR_CAN_SendNoTamper(&hcan, launchControl);
-          launchControlStart = uwTick;
+    if (nMot > 1000 && sendLaunch) {
+      if (isLaunchControlSetCnt >= 2 || isLaunchControlSetCnt <= -2) {
+        sendLaunch = false;
+      }
 
-
+      status = MMR_CAN_SendNoTamper(&hcan, launchControl);
     }
-}
-}
-          if (nMot > 9000) {
 
-                  launchControlStop = uwTick;
-                  launchControlCnt = 0;
-                  _launchControlData[0] = 0x0;
-                  state = RELEASED;
-                  MMR_CAN_SendNoTamper(&hcan, launchControlStopPacket);
+    switch (state) {
+    case START:
+    case PULL:
+    	if (nMot > 1000) {
+        if (uwTick - clutchMsgStart > 1) {
+          status = MMR_CAN_Send(&hcan, clutchPull);
+          clutchMsgStart = uwTick;
+        }
 
-                }
+        if (msg.header.messageId == MMR_CAN_MESSAGE_ID_CS_CLUTCH_PULL_OK) {
+          sendLaunch = true;
+          launchControlStart = uwTick;
+          state = PULLED;
+        }
+    	}
+      break;
 
+    case PULLED:
+      if (msg.header.messageId == MMR_CAN_MESSAGE_ID_ECU_ENGINE_FN2) {
+        if (buffer[6]) {
+          clutchMsgStart = uwTick;
+          clutchMsgCnt = 0;
+          state = GEAR_NOT_SET;
+        }
+      }
+      break;
 
+    case GEAR_NOT_SET:
+      if (!gear)
+        changeGear(true);
 
-//    if (readButton() == BTN_JUST_PRESSED) {
-//      isManual = !isManual;
-//      if (isManual) {
-//        for (int i = 0; i < 10; i++) {
-//          status = MMR_CAN_Send(&hcan, clutchSetManual);
-//          HAL_Delay(5);
-//        }
-//      }
-//      else {
-//        dacValue = DAC_0;
-//      }
-//
-//      stateChangeStart = uwTick;
-//      waitForStateChange = true;
-//      state = START;
-//    }
-//
-//    if (waitForStateChange && uwTick - stateChangeStart < 10000) {
-//      continue;
-//    }
-//
-//
-//    if (nMot > 1000 && sendLaunch && uwTick - launchControlStart > 25) {
-//      status = MMR_CAN_SendNoTamper(&hcan, launchControl);
-//      launchControlStart = uwTick;
-//    }
-//
-//    switch (state) {
-//    case START:
-//      if (isManual) {
-//    	  state = RELEASE;
-//      } else {
-//    	  state = PULL;
-//    	  debounce = 0;
-//      }
-//
-//
-//    case PULL:
-//    	if (nMot > 1000){
-//			if (uwTick - clutchMsgStart > 1) {
-//				status = MMR_CAN_Send(&hcan, clutchPull);
-//				clutchMsgStart = uwTick;
-//			}
-//
-//			if (msg.header.messageId == MMR_CAN_MESSAGE_ID_CS_CLUTCH_PULL_OK) {
-//			sendLaunch = true;
-//			launchControlStart = uwTick;
-//			state = PULLED;
-//			}
-//    	}
-//      break;
-//
-//    case PULLED:
-//      if (msg.header.messageId == MMR_CAN_MESSAGE_ID_ECU_ENGINE_FN2) {
-//        if (buffer[6]) {
-//          clutchMsgStart = uwTick;
-//          clutchMsgCnt = 0;
-//          state = GEAR_NOT_SET;
-//        }
-//      }
-//      break;
-//
-//    case GEAR_NOT_SET:
-//      if (!gear)
-//        changeGear(true);
-//
-//      state = GEAR_CHANGING;
-//      break;
-//
-//    case GEAR_CHANGING:
-//      if (gear) {
-//        changeGearStop = true;
-//        changeGear(true);
-//        HAL_GPIO_WritePin(GEAR_CHANGE_GPIO_Port, GEAR_CHANGE_Pin, GEAR_N_OFF);
-//        state = GEAR_CHANGED;
-//        break;
-//      }
-//
-//      if (!gear || !changeGear(false)) {
-//        waitMs(true, 2000);
-//        state = GEAR_NOT_CHANGED;
-//        break;
-//      }
-//
-//      break;
-//
-//    case GEAR_NOT_CHANGED:
-//      if (waitMs(false, 2000)) {
-//        HAL_GPIO_WritePin(GEAR_CHANGE_GPIO_Port, GEAR_CHANGE_Pin, GEAR_N_OFF);
-//        state = GEAR_NOT_SET;
-//      }
-//
-//      break;
-//
-//    case GEAR_CHANGED:
-//      HAL_GPIO_WritePin(GEAR_CHANGE_GPIO_Port, GEAR_CHANGE_Pin, GEAR_N_OFF);
-//      waitMs(true, 1000);
-//      state = LAUNCH_CONTROL_SET;
-//      break;
-//
-//    case LAUNCH_CONTROL_SET:
-//      dacValue = DAC_30;
-//      if (waitMs(false, 1000)) {
-//        state = APPS_30;
-//      }
-//      break;
-//
-//    case APPS_30:
-//      if (uwTick - clutchMsgStart > 1) {
-//        status = MMR_CAN_Send(&hcan, clutchRelease);
-//        clutchMsgStart = uwTick;
-//
-//        if (clutchMsgCnt++ >= 5 && nMot >= 6000) {
-//          state = RELEASE;
-//        }
-//      }
-//
-//      break;
-//
-//    case RELEASE:
-//      if (msg.header.messageId == MMR_CAN_MESSAGE_ID_CS_CLUTCH_RELEASE_OK) {
-//    	  firstrun=1;
-//        clutchMsgStart = uwTick;
-//        launchControlStop = uwTick;
-//        clutchMsgCnt = 0;
-//        launchControlCnt = 0;
-//        _launchControlData[0] = 0x0;
-//        state = RELEASED;
-//      }
-//      break;
-//
-//    case RELEASED:
-//      if (!isManual)
-//    	  dacValue = DAC_0;
-//
-//      if (clutchMsgCnt < 5 && uwTick - clutchMsgStart > 5) {
-//        status = MMR_CAN_Send(&hcan, clutchSetManual);
-//        clutchMsgStart = uwTick;
-//        clutchMsgCnt++;
-//      }
-//
-//      if (launchControlCnt < 1 && uwTick - launchControlStop > 100) {
-//        sendLaunch = false;
-//        launchControlStop = uwTick;
-//        launchControlCnt++;
-//      }
-//
-//      if (clutchMsgCnt >= 5 && launchControlCnt >= 1) {
-//        state = DONE;
-//      }
-//      break;
-//
-//    case DONE:
-//      break;
-//    }
+      state = GEAR_CHANGING;
+      break;
+
+    case GEAR_CHANGING:
+      if (gear) {
+        changeGearStop = true;
+        changeGear(true);
+        HAL_GPIO_WritePin(GEAR_CHANGE_GPIO_Port, GEAR_CHANGE_Pin, GEAR_N_OFF);
+        state = GEAR_CHANGED;
+        break;
+      }
+
+      if (!gear || !changeGear(false)) {
+        waitMs(true, 2000);
+        state = GEAR_NOT_CHANGED;
+        break;
+      }
+
+      break;
+
+    case GEAR_NOT_CHANGED:
+      if (waitMs(false, 2000)) {
+        HAL_GPIO_WritePin(GEAR_CHANGE_GPIO_Port, GEAR_CHANGE_Pin, GEAR_N_OFF);
+        state = GEAR_NOT_SET;
+      }
+
+      break;
+
+    case GEAR_CHANGED:
+      HAL_GPIO_WritePin(GEAR_CHANGE_GPIO_Port, GEAR_CHANGE_Pin, GEAR_N_OFF);
+      waitMs(true, 1000);
+      state = LAUNCH_CONTROL_SET;
+      break;
+
+    case LAUNCH_CONTROL_SET:
+      dacValue = DAC_30;
+      if (waitMs(false, 1000)) {
+        state = APPS_30;
+      }
+      break;
+
+    case APPS_30:
+      if (uwTick - clutchMsgStart > 1) {
+        status = MMR_CAN_Send(&hcan, clutchRelease);
+        clutchMsgStart = uwTick;
+
+        if (clutchMsgCnt++ >= 5 && nMot >= 6000) {
+          state = RELEASE;
+        }
+      }
+
+      break;
+
+    case RELEASE:
+      if (msg.header.messageId == MMR_CAN_MESSAGE_ID_CS_CLUTCH_RELEASE_OK) {
+    	  firstrun=1;
+        clutchMsgStart = uwTick;
+        launchControlStop = uwTick;
+        clutchMsgCnt = 0;
+        launchControlCnt = 0;
+        isLaunchControlSetCnt = 0;
+        state = RELEASED;
+      }
+      break;
+
+    case RELEASED:
+      if (clutchMsgCnt < 5 && uwTick - clutchMsgStart > 5) {
+        status = MMR_CAN_Send(&hcan, clutchSetManual);
+        clutchMsgStart = uwTick;
+        clutchMsgCnt++;
+      }
+
+      if (clutchMsgCnt >= 5) {
+        state = DONE;
+      }
+      break;
+
+    case DONE:
+      break;
+    }
 
     /* USER CODE END WHILE */
 
