0f68f506a8fb335c2d4e0322bbc6597838d9973f
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2022-06-17 13:30:05 +0200

    add comment

--- lib/filter/inc/lowpass.h
+++ lib/filter/inc/lowpass.h
@@ -13,8 +13,17 @@ typedef struct MmrLowPass MmrLowPass;
  * @return MmrLowPass freshly built object
  */
 MmrLowPass MMR_LowPass(float cutoff_freq, float time_period);
+
 void MMR_LOWPASS_SetCutoffFreq(MmrLowPass *obj, float cutoff_freq);
 void MMR_LOWPASS_SetTimePeriod(MmrLowPass *obj, float time_period);
+
+/**
+ * @brief filter input signal and return filtered value
+ * 
+ * @param obj MmrLowPass object reference
+ * @param input input signal
+ * @return uint32_t filtered signal
+ */
 uint32_t MMR_LOWPASS_Filter(MmrLowPass *obj, uint32_t input);
 
 #endif  // !LIB_FILTER_INC_LOWPASS_H_
