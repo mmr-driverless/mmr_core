372a5008285bd1906f87d76430303151dd7942c3
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2022-06-17 13:26:05 +0200

    kickstart lowpass filter library

---
+++ lib/filter/inc/lowpass.h
@@ -0,0 +1,20 @@
+#ifndef LIB_FILTER_INC_LOWPASS_H_
+#define LIB_FILTER_INC_LOWPASS_H_
+
+#include <stdint.h>
+
+typedef struct MmrLowPass MmrLowPass;
+
+/**
+ * @brief build a new low-pass filter object
+ * 
+ * @param cutoff_freq rad/s
+ * @param time_period time period
+ * @return MmrLowPass freshly built object
+ */
+MmrLowPass MMR_LowPass(float cutoff_freq, float time_period);
+void MMR_LOWPASS_SetCutoffFreq(MmrLowPass *obj, float cutoff_freq);
+void MMR_LOWPASS_SetTimePeriod(MmrLowPass *obj, float time_period);
+uint32_t MMR_LOWPASS_Filter(MmrLowPass *obj, uint32_t input);
+
+#endif  // !LIB_FILTER_INC_LOWPASS_H_
---
+++ lib/filter/lowpass.c
@@ -0,0 +1,46 @@
+#include "inc/lowpass.h"
+
+// Object definition
+typedef struct {
+    float cutoff_freq;
+    float time_period;
+    uint32_t last_output;
+} MmrLowPass;
+
+// Object constructor
+MmrLowPass MMR_LowPass(float cutoff_freq, float time_period) {
+    return (MmrLowPass) {
+        .cutoff_freq    = cutoff_freq,
+        .time_period    = time_period,
+        .output         = 0U
+    };
+}
+
+// Object Setters
+void MMR_LOWPASS_SetCutoffFreq(MmrLowPass *obj, float cutoff_freq) {
+    obj->cutoff_freq = cutoff_freq;
+}
+
+void MMR_LOWPASS_SetTimePeriod(MmrLowPass *obj, float time_period) {
+    obj->time_period = time_period;
+}
+
+// Object methods
+uint32_t MMR_LOWPASS_Filter(MmrLowPass *obj, uint32_t input) {
+    // TODO: implement, check diffs between lowpass16 and lowpass32
+    uint32_t output =
+        obj->last_output +
+        computeExpPortion(obj) * (input - obj->last_output);
+    updateLastOutput(obj, output);
+    return output;
+}
+
+// Helper functions
+static void updateLastOutput(MmrLowPass *obj, uint32_t output) {
+    obj->last_output = output;
+}
+
+static float computeExpPortion(MmrLowPass *obj) {
+    // TODO
+}
+
