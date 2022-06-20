f80a32bac594a0fd05cb59ef65b7247c45e57612
Stefano Calabretti <StefanoCalabretti@outlook.it>
2022-06-20 20:15:22 +0200

    Refactor

--- app/lib/filter/lowpass.c
+++ app/lib/filter/lowpass.c
@@ -6,23 +6,13 @@ static void updateLastOutput(MmrLowPass *obj, uint32_t output);
 static void updateExpCache(MmrLowPass *obj);
 static float computeExpPortion(MmrLowPass *obj);
 
-struct MmrLowPass {
-  float cutoffFreq;
-  float timePeriod;
-  MmrFilterBits bitNumber;
-  union {
-    uint16_t lastOutput16;
-    uint32_t lastOutput32;
-  };
-  float expCache;
-};
 
 MmrLowPass MMR_LowPass(float cutoffFreq, float timePeriod, MmrFilterBits bitNumber) {
   return (MmrLowPass) {
     .cutoffFreq = cutoffFreq,
     .timePeriod = timePeriod,
     .bitNumber = bitNumber,
-    .lastOutput32 = 0U,
+    .lastOutput = 0U,
     .expCache = exp(-timePeriod * cutoffFreq)
   };
 }
@@ -40,24 +30,21 @@ void MMR_LOWPASS_SetTimePeriod(MmrLowPass *obj, float timePeriod) {
 uint32_t MMR_LOWPASS_Filter(MmrLowPass *obj, uint32_t input) {
   uint32_t output =
     (float) getLastOutput(obj) +
-    computeExpPortion(obj) * (float) (input - obj->lastOutput32);
+    computeExpPortion(obj) *
+    (float) (input - getLastOutput(obj));
 
   updateLastOutput(obj, output);
   return output;
 }
 
 static uint32_t getLastOutput(MmrLowPass *obj) {
-  if (obj->bitNumber == MMR_FILTER_16)
-    return obj->lastOutput16;
-  else  // MMR_FILTER_32
-    return obj->lastOutput32;
+  return obj->lastOutput;
 }
 
 static void updateLastOutput(MmrLowPass *obj, uint32_t output) {
-  if (obj->bitNumber == MMR_FILTER_16)
-    obj->lastOutput16 = (uint16_t) output;
-  else  // MMR_FILTER_32
-    obj->lastOutput32 = output;
+  obj->lastOutput = obj->bitNumber == MMR_FILTER_16
+    ? (uint16_t) output
+    : output;
 }
 
 static void updateExpCache(MmrLowPass *obj) {
