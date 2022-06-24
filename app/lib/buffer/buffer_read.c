761ab8ba2ac05bac3e2ec883a6171ceeeb9a08da
Stefano Calabretti <StefanoCalabretti@outlook.it>
2022-06-24 19:39:39 +0200

    Update offsets

--- app/lib/buffer/buffer_read.c
+++ app/lib/buffer/buffer_read.c
@@ -24,15 +24,21 @@ static uint16_t readTwo(uint8_t *buffer, uint8_t offset, MmrEncoding encoding) {
 }
 
 static uint32_t readFour(uint8_t *buffer, uint8_t offset, MmrEncoding encoding) {
-  uint32_t first = readTwo(buffer, offset, encoding);
-  uint32_t second = readTwo(buffer, offset + 2, encoding);
+  uint8_t offset1 = encoding == MMR_ENCODING_BIG_ENDIAN ? 0 : 2;
+  uint8_t offset2 = encoding == MMR_ENCODING_BIG_ENDIAN ? 2 : 0;
+
+  uint32_t first = readTwo(buffer, offset + offset1, encoding);
+  uint32_t second = readTwo(buffer, offset + offset2, encoding);
 
   return first << 16 | second;
 }
 
 static uint64_t readEight(uint8_t *buffer, uint8_t offset, MmrEncoding encoding) {
-  uint64_t first = readFour(buffer, offset, encoding);
-  uint64_t second = readFour(buffer, offset + 2, encoding);
+  uint8_t offset1 = encoding == MMR_ENCODING_BIG_ENDIAN ? 0 : 4;
+  uint8_t offset2 = encoding == MMR_ENCODING_BIG_ENDIAN ? 4 : 0;
+
+  uint64_t first = readFour(buffer, offset + offset1, encoding);
+  uint64_t second = readFour(buffer, offset + offset2, encoding);
 
   return first << 32 | second;
 }
