1f655ff99b7c741776914d3597d8c3790f3197f4
Jeeg_ <73560519+Jeeg-robot@users.noreply.github.com>
2022-07-06 15:29:23 +0200

    res lib created

---
+++ test/launch_control/Core/Inc/res.h
@@ -0,0 +1,13 @@
+/*
+ * res.h
+ *
+ *  Created on: 6 lug 2022
+ *      Author: Maxmi
+ */
+
+#ifndef INC_RES_H_
+#define INC_RES_H_
+
+
+
+#endif /* INC_RES_H_ */
---
+++ test/launch_control/Core/Src/res.c
@@ -0,0 +1,8 @@
+/*
+ * res.c
+ *
+ *  Created on: 6 lug 2022
+ *      Author: Maxmi
+ */
+
+
